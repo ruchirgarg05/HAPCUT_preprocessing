4d27e09348b488477ca67a1b2cdb8cb63b4fce6e
ruchirgarg05 <ruchirgarg5@gmail.com>
2022-04-29 13:34:15 +0000

    high-memory

--- gen_hap.py
+++ gen_hap.py
@@ -43,24 +43,28 @@ def get_likelihood_read_given_hap(read, qual, H):
 
 
 class Block:
-    def __init__(self, cols):
+    def __init__(self, cols, likelihood_h1=None, likelihood_h2=None, read_indexes=None):
         self.cols = cols
-        read_indexes = []
+        self.likelihood_h1 = likelihood_h1
+        self.likelihood_h2 = likelihood_h2
         
-        read_idx = np.zeros(Frags.shape[0], dtype="bool")
-
-        for col in cols:
-            read_idx |=  ~np.isnan(Frags[:, col])
-
-        read_indexes = np.nonzero(read_idx)[0]
-        
-        self.read_indexes = read_indexes        
+        if read_indexes is None:
+            read_idx = np.zeros(Frags.shape[0], dtype="bool")
+            for col in cols:
+                read_idx |=  ~np.isnan(Frags[:, col])
+            read_indexes = np.nonzero(read_idx)[0]            
+        self.read_indexes = read_indexes
         # Reads that contain non nan values at atleast one of the cols.
     
     @property
     def hap(self):
-        return [H1.take(self.cols), H2.take(self.cols)]
+        self._hap = [H1.take(self.cols), H2.take(self.cols)]
+        return self._hap
 
+    @hap.setter
+    def hap(self, haplotype):
+        self._hap = haplotype
+ 
     @property
     def likelihood(self):
         return self.get_likelihood_given_haplotype() 
@@ -75,21 +79,40 @@ class Block:
 
 
     def get_likelihood_given_haplotype(self):
-        cols = self.cols
-        read_indexes = self.read_indexes
-        reads, quals =  Frags.take(read_indexes, axis=0), Quals.take(read_indexes, axis=0)
-        assert reads.shape[1] == quals.shape[1] == Frags.shape[1]
+        if self.likelihood_h1 is None or self.likelihood_h2 is None:            
+            cols = self.cols
+            read_indexes = self.read_indexes
+            # import ipdb;ipdb.set_trace()
+            reads, quals =  Frags.take(read_indexes, axis=0), Quals.take(read_indexes, axis=0)
+            assert reads.shape[1] == quals.shape[1] == Frags.shape[1]
 
-        reads, quals = reads.take(cols, axis=1), quals.take(cols, axis=1)
-        assert reads.shape[1] == len(cols)
-        hap = self.hap
+            reads, quals = reads.take(cols, axis=1), quals.take(cols, axis=1)
+            nan_reads = np.isnan(reads)
 
-        likelihood = 1.
-        for read, qual in zip(reads, quals):
-            # TODO: Make this sum of log(lik)
-            likelihood *=  get_likelihood_read_given_hap(read, qual, hap)
+            assert reads.shape[1] == len(cols)
 
-        return likelihood    
+            
+            hap = self.hap  
+            correct_r_h1 = np.array(reads == hap[0]).astype("int32")
+            correct_r_h2 = np.array(reads == hap[1]).astype("int32")
+            
+            like_h1 = (1 - 2 * quals) * correct_r_h1 + quals
+            like_h2 = (1 - 2 * quals) * correct_r_h2 + quals
+
+            # TODO: Handle nans carefully, here we are giving the prob that it is 
+            # coming from h1, h2 both as 1., maybe it would be suitable to give value as 0.5 
+            like_h1[nan_reads] = 1. 
+            like_h2[nan_reads] = 1.
+
+            self.likelihood_h1, self.likelihood_h2 = np.ones(Frags.shape[0]), np.ones(Frags.shape[0])
+
+            self.likelihood_h1[read_indexes], self.likelihood_h2[read_indexes] = like_h1.prod(axis=1), like_h2.prod(axis=1)
+
+        likelihoods =  (self.likelihood_h1 + self.likelihood_h2) / 2
+        likelihoods = np.prod(likelihoods)
+        return np.prod(likelihoods)
+
+
 
     def get_likelihood_of_merged(self, other):
         """
@@ -100,34 +123,69 @@ class Block:
         h11, h12 = self.hap
         h21, h22 = other.hap
 
-        hap1 = [np.concatenate([h11, h21]), np.concatenate([h12, h22])]
-        hap2 = [np.concatenate([h11, h22]), np.concatenate([h12, h21])]
         cols = list(set(self.cols + other.cols))
         read_indexes = np.unique( np.concatenate([self.read_indexes, other.read_indexes]) )
-        reads, quals =  Frags.take(read_indexes, axis=0), Quals.take(read_indexes, axis=0)
-        reads, quals = reads.take(cols, axis=1), quals.take(cols, axis=1)
 
-        l1, l2 = 1., 1. 
+        hap1 = [np.concatenate([h11, h21]), np.concatenate([h12, h22])]
+        hap2 = [np.concatenate([h11, h22]), np.concatenate([h12, h21])]
+        hap_phased, hap_unphased = hap1, hap2
+        _, __ = self.likelihood, other.likelihood
 
-        for read, qual in zip(reads, quals):
-            # TODO: Make this sum of log(lik)
-            l1 *= (get_likelihood_read_given_hap(read, qual, hap1))
-            l2 *= (get_likelihood_read_given_hap(read, qual, hap2))
+        lik_phased_h1 = self.likelihood_h1 * other.likelihood_h1
+        lik_phased_h2 = self.likelihood_h2 * other.likelihood_h2
 
-        if l1 > l2:
-            return l1, hap1, cols
+        lik_unphased_h1 = self.likelihood_h1 * other.likelihood_h2
+        lik_unphased_h2 = self.likelihood_h2 * other.likelihood_h1
 
-        return l2, hap2, cols
+        likelihood_phased = np.prod((lik_phased_h1 + lik_phased_h2)/2)
+        likelihood_unphased = np.prod((lik_unphased_h1 + lik_unphased_h2)/2)
+
+        if likelihood_phased > likelihood_unphased:
+            # the merged block should be in phase
+            merged_block = Block(cols, 
+            likelihood_h1=lik_phased_h1, 
+            likelihood_h2=lik_phased_h2, 
+            read_indexes=read_indexes)
+            merged_block.hap = hap_phased
+        
+        else:
+            merged_block = Block(cols, 
+            likelihood_h1=lik_unphased_h1,
+            likelihood_h2=lik_unphased_h2,
+            read_indexes=read_indexes)
+            merged_block.hap = hap_unphased
+        
+        return merged_block    
+
+
+
+
+        
+        
+        # reads, quals =  Frags.take(read_indexes, axis=0), Quals.take(read_indexes, axis=0)
+        # reads, quals = reads.take(cols, axis=1), quals.take(cols, axis=1)
+
+        
+        # l1, l2 = 1., 1. 
+
+        # for read, qual in zip(reads, quals):
+        #     # TODO: Make this sum of log(lik)
+        #     l1 *= (get_likelihood_read_given_hap(read, qual, hap1))
+        #     l2 *= (get_likelihood_read_given_hap(read, qual, hap2))
+
+        # if l1 > l2:
+        #     return l1, hap1, cols
+
+        # return l2, hap2, cols
 
 class Edge:
-    def __init__(self, lik, b1, b2, hap, cols):
+    def __init__(self, b1, b2, b1b2):
         # The factor by which the likelihood increases if blocks b1 and 
         # b2 are merged.
-        self.lik = lik
         self.u = b1
         self.v = b2
-        self.hap = hap
-        self.cols = cols
+        self.merged = b1b2
+        self.lik = self.merged.likelihood / (self.u.likelihood * self.v.likelihood)
 
     def __lt__(self, other):
         # Override the __lt__ for max heap
@@ -145,6 +203,7 @@ class BlockStore:
     
     def add(self, block):
         # O(1) insert
+        # O(logn) lookup
         if block in self.blocks:
             return
         sz = len(self.blocks)
@@ -153,11 +212,11 @@ class BlockStore:
         self.hashes.add(block.hash())
     
     def remove(self, block):
+        # O(logn) lookup
         # O(1) remove.
         index = self.hashd.get(block, None)
         if index is None:
-            return
-        # index = self.blocks.index(bloc)    
+            return  
 
         # If present remove the element from the hash
         self.hashes.remove(block.hash())   
@@ -180,6 +239,7 @@ def get_blocks_for_reads(fragments_path):
     #   - the two blocks have a read in common. 
     #   - merging these blocks where the probability of the P( b1 U b2  | h1 h2) / ( P(b1 | h1) * P(b2 | h2))
     import ipdb;ipdb.set_trace()
+    from tqdm import tqdm
     global Frags, Quals, H, H1, H2, block_store, heap
 
 
@@ -198,20 +258,22 @@ def get_blocks_for_reads(fragments_path):
 
     ipdb.set_trace()
     import pickle
-    if os.path.exists("heap.pkl"):
-        with open("heap.pkl", "rb") as fd:
-            heap = pickle.load(fd)
-    else:
+    # if os.path.exists("heap.pkl"):
+    #     with open("heap.pkl", "rb") as fd:
+    #         heap = pickle.load(fd)
+    if not len(heap):
+        # Calculate it.
 
-        for i in range(len(block_store.blocks)):
+        for i in tqdm(range(len(block_store.blocks))):
             for j in range(len(block_store.blocks)):
                 if i == j or not block_store.blocks[i].is_connected(block_store.blocks[j]):
                     # There are no reads connecting the two blocks as of now, 
                     # Hence no need to check whether the two should be merged. 
                     continue
-                l, hap, cols = block_store.blocks[i].get_likelihood_of_merged(block_store.blocks[j])
-                ed_val = l / (block_store.blocks[i].likelihood * block_store.blocks[j].likelihood)
-                edge = Edge(ed_val, block_store.blocks[i], block_store.blocks[j], hap, cols)
+                merged_block = block_store.blocks[i].get_likelihood_of_merged(block_store.blocks[j])
+                # ed_val = merged_block.likelihood / (block_store.blocks[i].likelihood * block_store.blocks[j].likelihood)
+                
+                edge = Edge(block_store.blocks[i], block_store.blocks[j], merged_block)
                 heap.append(edge)
     
     ipdb.set_trace()
@@ -219,7 +281,7 @@ def get_blocks_for_reads(fragments_path):
     
     while heap:
         max_edge = heapq.heappop(heap)
-        val, b1, b2, hap, cols = max_edge.lik, max_edge.u, max_edge.v, max_edge.hap, max_edge.cols
+        val, b1, b2, merged = max_edge.lik, max_edge.u, max_edge.v, max_edge.merged
         # Check both b1 and b2 are present in the block_store
         # if not simply continue as this is not a valid edge anymore.
         if b1.hash() not in block_store.hashes or b2.hash() not in block_store.hashes:
@@ -231,7 +293,7 @@ def get_blocks_for_reads(fragments_path):
         if np.log(val) < C*np.log(1 - 1/num_partitions):
             break
 
-        merge_blocks(b1, b2, hap, cols)
+        merge_blocks(b1, b2, merged)
         
     import ipdb;ipdb.set_trace()
     
@@ -239,9 +301,9 @@ def get_blocks_for_reads(fragments_path):
 
 
 
-def merge_blocks(b1, b2, hap, cols):
+def merge_blocks(b1, b2, b1b2):
     # Merge blocks and change the value of the haplotype block. 
-    new_block = Block(cols)
+    new_block = b1b2
     global block_store, H, H1, H2
     try:
         block_store.remove(b1)
@@ -252,20 +314,18 @@ def merge_blocks(b1, b2, hap, cols):
         block_store.remove(b2)  
      
     block_store.add(new_block)
-    H1[cols], H2[cols] = hap[0], hap[1]
+    H1[new_block.cols], H2[new_block.cols] = new_block.hap[0], new_block.hap[1]
     H = [H1, H2]
     
     # Go through all the connected blocks of b1 and b2 and
     # add new edges to the heap
-    new_block_likelihood = new_block.likelihood 
     for block in block_store.blocks:
         if block.is_connected(new_block) and not len(set(block.cols).intersection(new_block.cols)):
             try:
-                nl, nhap, ncols = new_block.get_likelihood_of_merged(block)
+                nb = new_block.get_likelihood_of_merged(block)
             except:
                 import ipdb;ipdb.set_trace()
                 pass
-            ed_val = nl / (new_block_likelihood * block.likelihood)
-            edge = Edge(ed_val, new_block, block, nhap, ncols)
+            edge = Edge(new_block, block, nb)
             heapq.heappush(heap, edge)
     return        
