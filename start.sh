ea6f029937c4fb645513ce0175a0e293a109372b
ruchirgarg05 <ruchirgarg5@gmail.com>
2022-05-04 16:50:34 -0700

    Create start.sh
    run start.sh at the start to make the machine compatible.

---
+++ start.sh
@@ -0,0 +1,8 @@
+pip install gdown
+pip install ipdb
+git clone https://github.com/ruchirgarg05/HAPCUT_preprocessing.git
+cd HAPCUT_preprocessing
+rm -r data
+gdown https://drive.google.com/drive/folders/1DmvistRCNKzzJ4af9LvhIWrD23kb1-95?usp=sharing --folder
+pip install torch
+pip install scikit-allel
